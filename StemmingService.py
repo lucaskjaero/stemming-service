54c80535adfd172927d759181d1f4a323c3cde47
Lucas Kjaero <lucas@lucaskjaero.com>
2018-04-26 00:06:05 -0700

    First addition of segmenter

--- StemmingService.py
+++ StemmingService.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
+from chinese.Segmenter import words_in_text
+
 app = Flask(__name__)
 api = Api(app)
 
 class DocumentHandler(Resource):
     def post(self, language):
+        if (language == "chinese"):
+            words = words_in_text("Text")
+
         return {"message": "Parsing not supported yet", "language": language}
 
 api.add_resource(DocumentHandler, '/v1/document/<string:language>')
