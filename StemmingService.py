d71800348c07d9b23c20c6bd544cd1ff2fba1237
Lucas Kjaero <lucas@lucaskjaero.com>
2018-04-25 23:41:02 -0700

    Correction

--- StemmingService.py
+++ StemmingService.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/v1/document/<language>', methods=['GET', 'POST'])
+def process_document(language):
+    return "Processing document in " + language
