32ba7b6e136bef654fd3f5d4074891e348023ef7
Lucas Kjaero <lucas@lucaskjaero.com>
2018-04-26 07:57:40 -0700

    Chinese status

--- StemmingService.py
+++ StemmingService.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 from chinese.Segmenter import words_in_text
@@ -9,9 +9,10 @@ api = Api(app)
 class DocumentHandler(Resource):
     def post(self, language):
         if (language == "chinese"):
-            words = words_in_text("Text")
+            text = request.form['text']
+            words = list(words_in_text(text))
 
-        return {"message": "Parsing not supported yet", "language": language}
+        return {"words": words, "language": language}
 
 api.add_resource(DocumentHandler, '/v1/document/<string:language>')
 
