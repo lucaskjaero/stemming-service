d53ef406278e80a440b42e582d8f7186d8f65cbc
Lucas Kjaero <lucas@lucaskjaero.com>
2018-04-25 23:59:40 -0700

    Add REST endpoints

--- StemmingService.py
+++ StemmingService.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+class DocumentHandler(Resource):
+    def post(self, language):
+        return {"message": "Parsing not supported yet", "language": language}
 
-@app.route('/v1/document/<language>', methods=['GET', 'POST'])
-def process_document(language):
-    return "Processing document in " + language
+api.add_resource(DocumentHandler, '/v1/document/<string:language>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
