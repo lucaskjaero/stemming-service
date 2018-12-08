7cc055124f1852c37918ef7f10ded49503c81a8f
Lucas Kjaero <lucas@lucaskjaero.com>
2018-12-07 23:20:25 -0800

    Enable spanish, set up path base rewriting

--- StemmingService.py
+++ StemmingService.py
@@ -1,12 +1,16 @@
+import os
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 
 from chinese.Segmenter import segment_chinese
 from english.Segmenter import segment_english
-#from spanish.Segmenter import segment_spanish
+from spanish.Segmenter import segment_spanish
 
 app = Flask(__name__)
 api = Api(app)
+
+API_BASE = os.getenv('APPLICATION_ROOT', "/")
 
 
 class DocumentHandler(Resource):
@@ -25,13 +29,13 @@ class DocumentHandler(Resource):
                 words = segment_chinese(text)
             elif language == "english":
                 words = segment_english(text)
-            #elif language == "spanish":
-                #words = segment_spanish(text)
+            elif language == "spanish":
+                words = segment_spanish(text)
             else:
                 status = "ERROR"
                 message = "Language %s has not been implemented yet." % language
                 response_code = 400
-        except:
+        except Exception as e:
             status = "ERROR"
             message = "An error occurred"
             response_code = 500
@@ -44,7 +48,7 @@ class HealthHandler(Resource):
         return "Stemming service is up", 200
 
 
-api.add_resource(DocumentHandler, '/v1/<string:language>/document')
+api.add_resource(DocumentHandler, API_BASE + '/stemming/v1/<string:language>/document')
 api.add_resource(HealthHandler, '/')
 
 if __name__ == '__main__':
